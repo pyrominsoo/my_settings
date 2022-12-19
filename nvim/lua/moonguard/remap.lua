d8c1a682b1b18352f7f1468ea0fc8b5edfaaa688
Min Soo Kim <min.soo.kim@wdc.com>
2022-12-19 23:15:11 +0000

    Removed lsp. Found it to be only distraction, at least at the moment

--- nvim/lua/moonguard/remap.lua
+++ nvim/lua/moonguard/remap.lua
@@ -20,7 +20,7 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 
 vim.keymap.set("n", "q", "<nop>")
 vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+-- vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
