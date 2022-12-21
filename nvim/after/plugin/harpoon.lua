ee671114c202c33f61f2a1db86c4f34a65f447bc
Min Soo Kim <min.soo.kim@wdc.com>
2022-12-21 23:26:46 +0000

    Changed <leader>aj and ak to iterate through harpoon list instead of going to first and second items.

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -4,5 +4,5 @@ local ui = require("harpoon.ui")
 vim.keymap.set("n", "<leader>aa", mark.add_file)
 vim.keymap.set("n", "<leader>ae", ui.toggle_quick_menu)
 
-vim.keymap.set("n", "<leader>aj", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<leader>ak", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<leader>aj", function() ui.nav_next() end)
+vim.keymap.set("n", "<leader>ak", function() ui.nav_prev() end)
