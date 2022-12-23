75c9ba274fcc39fc01f568140e7ce23ca11c89ac
Min Soo Kim <minsk1@uci.edu>
2022-12-23 07:56:20 -0800

    Remapped <C-j> and <C-k>

--- nvim/lua/moonguard/remap.lua
+++ nvim/lua/moonguard/remap.lua
@@ -6,8 +6,8 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-j>", "<C-d>zz")
+vim.keymap.set("n", "<C-k>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
@@ -22,9 +22,6 @@ vim.keymap.set("n", "s", "<nop>")
 vim.keymap.set("n", "q", "<nop>")
 vim.keymap.set("n", "Q", "<nop>")
 -- vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
@@ -43,11 +40,6 @@ vim.keymap.set("n", "<leader>wj", "<C-w>j")
 vim.keymap.set("n", "<leader>wk", "<C-w>k")
 vim.keymap.set("n", "<leader>wh", "<C-w>h")
 vim.keymap.set("n", "<leader>wl", "<C-w>l")
-
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
 
 vim.keymap.set("n", "<leader>k", "<C-u>")
 vim.keymap.set("n", "<leader>j", "<C-d>")
@@ -73,8 +65,8 @@ vim.keymap.set("n", "<F9>", ":TagbarToggle<CR>")
 
 vim.keymap.set("n", "[g", ":lprevious<CR>")
 vim.keymap.set("n", "]g", ":lnext<CR>")
-vim.keymap.set("n", "[f", ":cprev<CR>")
-vim.keymap.set("n", "]f", ":cnext<CR>")
+vim.keymap.set("n", "[f", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "]f", "<cmd>cnext<CR>zz")
 
 vim.keymap.set("n", "<leader>pt", ":ts ")
 
