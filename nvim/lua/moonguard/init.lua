d53ed570d398d3a061bf1fb44946edd48bcffc2a
Min Soo Kim <min.soo.kim@wdc.com>
2023-03-24 18:02:15 +0000

    Removed autocommenting

--- nvim/lua/moonguard/init.lua
+++ nvim/lua/moonguard/init.lua
@@ -59,3 +59,6 @@ vim.api.nvim_create_user_command(
 
 vim.cmd([[set grepprg=rg\ --vimgrep\ --no-heading]])
 vim.cmd([[set grepformat=%f:%l:%c:%m,%f:%l:%m]])
+
+-- disable autocomment
+vim.cmd([[autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o]])
