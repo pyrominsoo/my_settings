68849290ce3eea380d37140fb613f2267323c99b
Min Soo Kim <minsk1@uci.edu>
2023-04-04 14:35:31 -0700

    Added shortcut for Telescope. Changed alt file from <leader>leader to Backspace

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,4 +1,5 @@
 local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ts', '<cmd>Telescope<CR>', {})
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ps', builtin.git_files, {})
 vim.keymap.set('n', '<leader>pt', builtin.tags, {})
--- nvim/lua/moonguard/remap.lua
+++ nvim/lua/moonguard/remap.lua
@@ -78,7 +78,7 @@ vim.keymap.set("n", "<F2>", "i<CR><ESC>")
 vim.keymap.set("i", "<F2>", "<ESC>a<CR><ESC>")
 vim.keymap.set("n", "<leader>wt", [[:.s/.*/===== \0 =====/<CR>]])
 
-vim.keymap.set("n", "<leader><leader>", "<C-^>")
+vim.keymap.set("n", "<Backspace>", "<C-^>")
 
 vim.keymap.set("n", "<leader>,cl", [[: -1read $HOME/.config/nvim/snippets/class.cpp<CR>]])
 vim.keymap.set("n", "<leader>,cr", [[: -1read $HOME/.config/nvim/snippets/copyright.cpp<CR>]])
