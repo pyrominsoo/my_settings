ce013ea8413b640963bf42fe3614085f192f8d32
Min Soo Kim <minsk1@uci.edu>
2024-06-23 15:08:57 -0700

    Updated plot.py

--- plot.py
+++ plot.py
@@ -1,6 +1,26 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
+byte_per_val = 4
+# Initialize min and max memory addresses
+min_point = 23702854340464
+
+# Create a figure and axis
+fig, ax = plt.subplots(figsize=(15, 8))
+
+x_max = 1024
+y_max = 1024
+x_width = 1
+y_width = 1
+ax.set_xlim(0, x_max+x_width)
+ax.set_ylim(0, y_max+y_width)
+
+
+class Point:
+    def __init__(self, pos, color):
+        self.pos = pos
+        self.color = color
+
 
 class Entry:
     # Assumes the fields in this order
@@ -9,39 +29,68 @@ class Entry:
     def __init__(self, fields):
         if (len(fields) != 18):
             raise ValueError("fields has invalid entries")
-        self.layer = fields[0]
+        self.layer = int(fields[0])
         self.trans_a = fields[1]
         self.trans_b = fields[2]
         self.trans_c = fields[3]
-        self.contig_a = fields[4]
-        self.contig_b = fields[5]
-        self.contig_c = fields[6]
-        self.m = fields[7]
-        self.n = fields[8]
-        self.k = fields[9]
-        self.alpha = fields[10]
-        self.a = int(fields[11], 16)
-        self.lda = fields[12]
-        self.b = int(fields[13], 16)
-        self.ldb = fields[14]
-        self.beta = fields[15]
-        self.c = int(fields[16], 16)
-        self.ldc = fields[17]
+        self.contig_a = int(fields[4])
+        self.contig_b = int(fields[5])
+        self.contig_c = int(fields[6])
+        self.m = int(fields[7])
+        self.n = int(fields[8])
+        self.k = int(fields[9])
+        self.alpha = int(fields[10])
+        self.a = int(fields[11], 16) // byte_per_val
+        self.lda = int(fields[12])
+        self.b = int(fields[13], 16) // byte_per_val
+        self.ldb = int(fields[14])
+        self.beta = int(fields[15])
+        self.c = int(fields[16], 16) // byte_per_val
+        self.ldc = int(fields[17])
+        if (min(self.a, self.b, self.c) < min_point):
+            raise ValueError("Min address violated")
+        self.a = self.a - min_point
+        self.b = self.b - min_point
+        self.c = self.c - min_point
+        if (self.layer == 0):
+            color = 'b'
+        elif (self.layer == 1):
+            color = 'g'
+        elif (self.layer == 2):
+            color = 'c'
+        else:
+            raise ValueError("Unexpected self.layer value")
+        self.points_a = self.__ReturnPoints(
+            self.a // 4, self.lda,
+            (self.k if self.trans_a == "TransposeA" else self.m),
+            (self.m if self.trans_a == "TransposeA" else self.k), color)
+        if (self.layer == 0):
+            color = 'r'
+        elif (self.layer == 1):
+            color = 'm'
+        elif (self.layer == 2):
+            color = 'y'
+        else:
+            raise ValueError("Unexpected layer value")
+        self.points_b = self.__ReturnPoints(
+            self.b // 4, self.ldb,
+            (self.n if self.trans_b == "TransposeB" else self.k),
+            (self.k if self.trans_b == "TransposeB" else self.n), color)
+        color = 'k'
+        self.points_c = self.__ReturnPoints(
+            self.c // 4, self.ldc, self.m, self.n, color)
+        self.points = self.points_a + self.points_b + self.points_c
 
-    def ReturnMempoints(self):
-        return [self.a]
-
-
-# Initialize min and max memory addresses
-min_addr = 94811417406272
-max_addr = -float('inf')
-
-# Create a figure and axis
-fig, ax = plt.subplots(figsize=(30, 8))
-
-y_max = 4096
-x_width = 32
-y_width = 32
+    def __ReturnPoints(self, begin_point, ld, param1, param2, color):
+        retlist = []
+        pos = begin_point
+        # retlist.append(Point(pos, color))
+        for j in range(0, param2):
+            for i in range(0, param1):
+                retlist.append(Point(pos, color))
+                pos += 1
+            pos += ld
+        return retlist
 
 
 # Read file
@@ -49,43 +98,17 @@ with open('trace', 'r') as f:
     trace = f.readlines()
 
 
-total_addresses = []
 for entry in trace:
     fields = entry.strip().split()
     curr_entry = Entry(fields)
-    # Generate individual points
-    curr_mempoints = curr_entry.ReturnMempoints()
-    print(curr_mempoints)
-
-    if (min(curr_mempoints) < min_addr):
-        raise ValueError("min_addr violated")
-    elif (max(curr_mempoints) > max_addr):
-        # Recalculate the range and redraw the past points
-        max_addr = max(curr_mempoints)
-        addr_range = max_addr - min_addr
-        x_max = addr_range // y_max
-        ax.clear()
-        ax.set_xlim(0, x_max+x_width)
-        ax.set_ylim(0, y_max+y_width)
-        for addr_entry in total_addresses:
-            xcoord = addr_entry // y_max
-            ycoord = addr_entry % y_max
-            sq = patches.Rectangle(
-                (xcoord, ycoord), x_width, y_width, fill=True)
-            ax.add_patch(sq)
-    # subtract the offset
-    curr_mempoints = [point - min_addr for point in curr_mempoints]
-    for point in curr_mempoints:
-        xcoord = point // y_max
-        ycoord = point % y_max
-        print(point)
-        print(xcoord)
-        print(ycoord)
-        print(addr_range)
-        sq = patches.Rectangle((xcoord, ycoord), x_width, y_width, fill=True)
+    for point in curr_entry.points:
+        pos = point.pos
+        xcoord = pos // y_max
+        ycoord = pos % y_max
+        sq = patches.Rectangle((xcoord, ycoord), x_width,
+                               y_width, fill=True, color=point.color)
         ax.add_patch(sq)
-    total_addresses.extend(curr_mempoints)
-    plt.pause(0.5)
+    plt.pause(0.2)
 
 # # Get an array of memory addresses
 # addresses = np.random.randint(0, 1000, size=100)
