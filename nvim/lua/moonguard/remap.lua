f7fafa6823f6dab48dcf6f1c4fffe900c1b1faf7
Min Soo Kim <min.soo.kim@sandisk.com>
2024-12-30 12:50:50 -0800

    nvim and telescope update

--- nvim/lua/moonguard/remap.lua
+++ nvim/lua/moonguard/remap.lua
@@ -64,7 +64,6 @@ vim.keymap.set("n", "<F6>", ":cd ..<CR> :pwd<CR>")
 vim.keymap.set("n", "<F7>", ":cd %:p:h<CR> :pwd<CR>")
 vim.keymap.set("n", "<F8>", ":set invpaste paste?<CR>")
 vim.keymap.set("n", "<leader>sp", ":setlocal paste!<CR>")
-vim.cmd(':set pastetoggle=<F8>')
 vim.keymap.set("n", "<F9>", ":call ToggleQuickFix()<CR>")
 
 vim.keymap.set("n", "[g", ":lprevious<CR>")
