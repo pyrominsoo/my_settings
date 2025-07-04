5c81646f2fa296e1d4894d2e71f7df4532224c9d
Min Soo Kim <minsk1@uci.edu>
2025-07-04 03:35:48 -0700

    Revised the Attach commands to prefill ~/Down

--- nvim/lua/moonguard/abbr.lua
+++ nvim/lua/moonguard/abbr.lua
@@ -198,7 +198,7 @@ end, {
   desc = "Copy file from given path to $CURR_DIR/$SUBDIR and insert link as [[./filename]] or [[./filename_N.ext]]"
 })
 
-vim.keymap.set('n', '<leader>;a', ':Attach ', { noremap = true })
-vim.keymap.set('n', '<leader>;b', ':Bttach ', { noremap = true })
-vim.keymap.set('n', '<leader>;c', ':Cttach ', { noremap = true })
+vim.keymap.set('n', '<leader>;a', ':Attach ~/Down', { noremap = true })
+vim.keymap.set('n', '<leader>;b', ':Bttach ~/Down', { noremap = true })
+vim.keymap.set('n', '<leader>;c', ':Cttach ~/Down', { noremap = true })
 
