91099e7a23116bc5e26653f95610cfc15a284088
Min Soo Kim <minsk1@uci.edu>
2025-08-09 09:07:43 -0700

    Moved headers.lua to init.lua; added <leader>;m

--- nvim/lua/moonguard/init.lua
+++ nvim/lua/moonguard/init.lua
@@ -1,5 +1,4 @@
 require("moonguard.set")
-require("moonguard.headers")
 require("moonguard.remap")
 require("moonguard.abbr")
 
@@ -886,4 +885,99 @@ vim.keymap.set('n', '<leader>;j', function()
     end
   end)
 end, { desc = "Add/subtract days to date in line or insert date" })
+
+
+
+
+
+
+local function decorate_line_with_equals()
+  local line = vim.api.nvim_get_current_line()
+  local prefix, sentence, suffix = line:match('^(=+)%s(.-)%s(=+)$')
+  if prefix and suffix then
+    prefix = prefix .. "="
+    suffix = suffix .. "="
+    line = prefix .. " " .. sentence .. " " .. suffix
+  else
+    line = "= " .. line .. " ="
+  end
+  vim.api.nvim_set_current_line(line)
+end
+
+local function reduce_equals_decoration()
+  local line = vim.api.nvim_get_current_line()
+  local prefix, sentence, suffix = line:match('^(=+)%s(.-)%s(=+)$')
+  if prefix and suffix then
+    if #prefix == 1 then
+      -- Remove decoration entirely
+      line = sentence
+    else
+      prefix = prefix:sub(1, -2) -- remove one '='
+      suffix = suffix:sub(1, -2)
+      line = prefix .. " " .. sentence .. " " .. suffix
+    end
+    vim.api.nvim_set_current_line(line)
+  end
+end
+
+-- Helper function to insert text at cursor in normal or insert mode
+local function insert_at_cursor(str)
+  local mode = vim.api.nvim_get_mode().mode
+  if mode:sub(1,1) == 'i' then
+    -- Insert mode: insert text directly
+    vim.api.nvim_put({str}, 'c', true, true)
+  else
+    -- Normal mode: insert text after cursor
+    vim.api.nvim_put({str}, 'c', true, false)
+  end
+end
+
+
+-- Helper: decorate N times
+local function decorate_n_times(n)
+  for _ = 1, n do
+    decorate_line_with_equals()
+  end
+end
+
+for i = 1, 5 do
+  vim.keymap.set('n', '<leader>;'..i, function() decorate_n_times(i) end, { noremap = true, silent = true, desc = "Decorate line with "..i.." =" })
+end
+
+
+vim.keymap.set('n', '=', decorate_line_with_equals, { noremap = true, silent = true })
+vim.keymap.set('n', '-', reduce_equals_decoration, { noremap = true, silent = true })
+
+
+
+
+
+
+vim.keymap.set('n', '<leader>;m', function()
+  local buf = 0
+  local row = vim.api.nvim_win_get_cursor(0)[1]
+
+  -- Get the current line text
+  local line = vim.api.nvim_buf_get_lines(buf, row - 1, row, false)[1]
+
+  -- Duplicate current line below
+  vim.api.nvim_buf_set_lines(buf, row, row, false, {line})
+
+  -- Move cursor to first line and call decorate_n_times(5)
+  vim.api.nvim_win_set_cursor(0, {row, 0})
+  decorate_n_times(5)
+
+  -- Get second line text, remove all whitespace
+  local second_line = vim.api.nvim_buf_get_lines(buf, row, row + 1, false)[1]
+  second_line = second_line:gsub("%s+", "")
+  -- Set cleaned second line back
+  vim.api.nvim_buf_set_lines(buf, row, row + 1, false, {second_line})
+
+  -- Move cursor to second line and call create_link_from_path_under_cursor()
+  vim.api.nvim_win_set_cursor(0, {row + 1, 0})
+  create_link_from_path_under_cursor()
+end)
+
+
+
 
